8ccd570b44e9a604f2e9d7b5d366e4842ed9fe5f
moulina pradhan <pmoulina76@gmail.com>
2020-08-21 23:21:23 +0530

    error

---
+++ src/actions/index.js
@@ -0,0 +1,63 @@
+export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_FAVOURITE = "ADD_FAVOURITE";
+export const REMOVE_FAVOURITE = "REMOVE_FAVOURITE";
+export const SHOW_FAVOURITES = "SHOW_FAVOURITES";
+export const ADD_MOVIE_TO_LIST = "ADD_MOVIE_TO_LIST";
+export const ADD_SEARCH_RESULT = "ADD_SEARCH_RESULT";
+
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies: movies,
+  };
+}
+
+export function addFovourite(movie) {
+  return {
+    type: ADD_FAVOURITE,
+    movie: movie,
+  };
+}
+
+export function removeFavourite(movie) {
+  return {
+    type: REMOVE_FAVOURITE,
+    movie: movie,
+  };
+}
+
+export function showFavourites(val) {
+  return {
+    type: SHOW_FAVOURITES,
+    showFavourites: val,
+  };
+}
+
+export function addMovieToList(movie) {
+  return {
+    type: ADD_MOVIE_TO_LIST,
+    movie: movie,
+  };
+}
+
+export function handleMovieSearch(movie) {
+  const url = `https://www.superheroapi.com/api.php/1170962379927864/search/${movie}`;
+
+  return function (dispatch) {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((movie) => {
+        console.log(movie);
+        dispatch(addMovieSearch(movie));
+      });
+  };
+}
+
+export function addMovieSearch(movie) {
+  return {
+    type: ADD_SEARCH_RESULT,
+    movie: movie,
+  };
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { addMovieToList, handleMovieSearch } from "../actions";
+import { connect } from "react-redux";
+
+class Navbar extends React.Component {
+  handleAddToMovies = (movie) => {
+    this.props.dispatch(addMovieToList(movie));
+  };
+  handleSearch = () => {
+    var searchText = document.getElementById("input").value;
+    this.props.dispatch(handleMovieSearch(searchText));
+    console.log("this.prps.search.result", this.props.search.result);
+  };
+  render() {
+    return (
+      <div className="nav">
+        <div className="search-container">
+          <input id="input" />
+          <button id="search-btn" onClick={this.handleSearch}>
+            Search
+          </button>
+
+          {this.props.search.showSearchResults && (
+            <div className="search-results">
+              <div className="search-result">
+                <img src={this.props.search.result.results.image} />
+                <div className="movie-info">
+                  <span>{this.props.search.result.results.name}</span>
+                  <button
+                    onClick={() =>
+                      this.handleAddToMovies(this.props.search.result)
+                    }
+                  >
+                    Add To Movies
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    search: state.search,
+  };
+}
+
+const connectedNavbarComponent = connect(mapStateToProps)(Navbar);
+
+export default connectedNavbarComponent;
